1918bb571e67d5bf874bc1cd49626aa85a6878d2
yvens <yvensre@gmail.com>
2023-01-23 07:27:27 +0100

    Restore Call On Collision

---
+++ Unity Events/CallOnCollision.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Original Author: Bugulet
+/// Revisor: YvensFaos
+/// </summary>
+public class CallOnCollision : MonoBehaviour
+{
+    [SerializeField] private bool onCollision = true;
+    [SerializeField] private bool onTrigger;
+
+    [Tooltip("What object type do you want this to work on, empty for any object")] [SerializeField]
+    private string objectTag;
+
+    [SerializeField] private UnityEvent eventToTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (onTrigger)
+        {
+            Resolve(other.gameObject);    
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (onCollision)
+        {
+            Resolve(other.gameObject);    
+        }
+    }
+
+    private void Resolve(GameObject other)
+    {
+        if (objectTag.Length > 0)
+        {
+            if (other.CompareTag(objectTag))
+            {
+                eventToTrigger.Invoke();
+            }
+        }
+        else
+        {
+            eventToTrigger.Invoke();
+        }
+    }
+}
